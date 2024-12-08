23ad7fe46c57808ccc19190705b601e76d930da0
trevizanebuonafinamc <trevizanebuonafinamc@gmail.com>
2024-12-08 21:54:09 +0000

    feat: navbar js

---
+++ app.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toogle')
+const nav = document.querySelector('nav')
+
+menuToggle.addEventListener('click', () =>{
+    nav.classList.toggle('active')
+});
